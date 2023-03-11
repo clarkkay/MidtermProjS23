a375c0d91c563b45afcb567463e5e9b711f7e2e1
Kayley <kclark3691@gmail.com>
2023-03-11 10:13:19 -0600

    rasp commits

--- js/rasp3_csv.js
+++ js/rasp3_csv.js
@@ -1,14 +1,11 @@
-$(document).ready(function() {
-    $.get('rasp3_data.csv', function(data) {
-      // Split the CSV data into an array of lines
-      var lines = data.split('\n');
-      
-      // Split the first line into an array of values
-      var values = lines[0].split(',');
-      
-      // Get the first value and insert it into the card text
-      var r3cardtxt = $('.card-text');
-      r3cardtxt.text(values[0]);
+function getLastData(csvFile) {
+    return csvFile.text().then(data => {
+      const rows = data.trim().split('\n');
+      const lastRow = rows[rows.length - 1];
+      const lastData = lastRow.split(',').find(cell => cell.startsWith('44.0'));
+      return lastData;
     });
-  });
+  }
+  
+  
   
