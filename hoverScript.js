2569accdd8f489958d08de08e3193c64f5431d96
M-M5 <67450044+M-M5@users.noreply.github.com>
2023-03-11 21:43:56 -0600

    Updated Index
    Worked on the way the cards work

---
+++ hoverScript.js
@@ -0,0 +1,3 @@
+Object.onmouseover = function() {
+    // Fade the current card. On top of the fade add a description for the given card
+}
---
+++ js/customJS.js
@@ -0,0 +1,24 @@
+Object.onmouseover = function() {
+    var currentCard = this;
+    if (currentCard == "micro-sd") {
+        var title = "Micro SD Card";
+        var description = "A micro SD card is a small flash memory card that is used to store data. It is used in many devices such as phones, cameras, and computers.";
+    }
+    else if (currentCard == "micro-usb") {
+        var title = "Micro USB";
+        var description = "A micro USB is a small USB connector that is used to connect devices to a computer. It is used in many devices such as phones, cameras, and computers.";
+    }
+    else if (currentCard == "DHT11") {
+        var title = "DHT11";
+        var description = "A DHT11 is a temperature and humidity sensor that is used to measure the temperature and humidity of the environment. It is used in many devices such as phones, cameras, and computers.";
+    }
+    else if (currentCard == "rPi4") {
+        var title = "Raspberry Pi 4";
+        var description = "A Raspberry Pi 4 is a small computer that is used to run programs. It is used in many devices such as phones, cameras, and computers.";
+    }
+    else if (currentCard == "rPi3") {
+        var title = "Raspberry Pi 3";
+        var description = "A Raspberry Pi 3 is a small computer that is used to run programs. It is used in many devices such as phones, cameras, and computers.";
+    }
+    return title, description;
+}
